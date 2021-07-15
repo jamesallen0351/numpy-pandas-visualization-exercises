52d1fe092f1ed1fdb61ace87cf912f5cb76b7021
jamesallen0351 <james.allen0351@gmail.com>
2021-07-15 11:58:42 -0500

    adding numpy notes and exercises

---
+++ numpy_exercises.py
@@ -0,0 +1,24 @@
+# numpy_exercises
+
+insert numpy as np 
+
+a = np.array([4, 10, 12, 23, -2, -1, 0, 0, 0, -6, 3, -7])
+
+# 1 How many negative numbers are there?
+
+# 2 How many positive numbers are there?
+
+# 3 How many even positive numbers are there?
+
+# 4 If you were to add 3 to each data point, how many positive numbers would there be?
+
+# 5 If you squared each number, what would the new mean and standard deviation be?
+
+# 6 A common statistical operation on a dataset is centering. 
+# This means to adjust the data such that the mean of the data is 0. 
+# This is done by subtracting the mean from each data point. Center the data set.
+
+# 7 Calculate the z-score for each data point. Recall that the z-score is given by:
+
+# 8 Copy the setup and exercise directions from More Numpy Practice into your numpy_exercises.py and add your solutions.
+
