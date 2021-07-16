dd5d235d575dc9f940487ec33e47d219b65050e6
jamesallen0351 <james.allen0351@gmail.com>
2021-07-16 10:46:05 -0500

    add panda series exercises in progress

---
+++ pandas_series.py
@@ -0,0 +1,15 @@
+# panda exercises
+
+# Part 1
+
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+# Use pandas to create a Series named fruits from the following list:
+
+fruits = pd.Series(["kiwi", "mango", "strawberry", "pineapple", "gala apple", "honeycrisp apple", "tomato", "watermelon", "honeydew", "kiwi", "kiwi", "kiwi", "mango", "blueberry", "blackberry", "gooseberry", "papaya"])
+
+print(fruits)
+
